f37923b74c2f9a0c63f4afd4f3be5db5ee98cd37
Joaquin <clcflecha@gmail.com>
2024-09-09 10:17:39 -0400

    Primer commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import { AddCategory, GifGrid } from "./components";
+
+export const GifExpertApp = () => {
+
+    
+    const [categories, setCategories] = useState([ 'One Punch' ]);
+
+    // 3- onAddCategory recibe el valor:newCategory por medio de OnNewCategory y lo usa para cargarlo
+    // a traves de SetCategories  
+    const onAddCategory = (newCategory) => {
+        // valorant
+
+        if ( categories.includes(newCategory) ) return;
+
+        setCategories([ newCategory, ...categories ])
+        // (en este punto hemos terminado de cargar la nueva categoria dentro de las categorias)
+    }
+
+
+  return (
+    <>
+        <h1>GifExpertApp</h1>
+        
+        <AddCategory 
+            // 1- AddCategory es la barra de texto en donde intruducimos 
+            // la nueva categoria nueva para actualizar "categorias"
+
+            onNewCategory={ (value) => onAddCategory(value) }    
+            // 2- OnNewCategory es la referencia que usaremos dentro de AddCategory
+            // Una vez el usuario carga la nueva categoria se llama la funcion onAddCategory
+            // onAddCategory esta en el componente Principal
+        />
+        {
+            // 5- Una vez que "categorias" está actualizada, toca recorrer las categorias que
+            // hay dentro.
+
+            categories.map( (category) => (
+                // Recorremos "categorias" y pasamos cada categoria que hay dentro de "categorias"
+                // como referencia del map, para añadirle una key e imprimir cada "category"
+                <GifGrid 
+                key={ category }
+                category={ category }
+                />
+            ))
+            
+            // Proceso del componente principal
+            /*
+                - Renderiza el componente, aparece el titulo <h1>, la barra y las categorias 
+                    (Al principio las categorias en total son 1, "One Punch") 
+                    ('One Punch' terminó su trabajo al renderizarlo)
+
+                - El usuario escribe una nueva category y toca "Enter" -> Esto devuelve un valor
+
+                - El valor es utilizado para el setCategories. (instantaneamente se vuelve a
+                    renderizar el componente al usar useState):
+                    *<h1>
+                    *Barra (Pero no realiza ninguna acción porque está esperando a que toques enter)
+                    *Realiza el categories.map (Y para ese momento "categories" ya tendrá una
+                        nueva "categoria")
+                
+                - categories.map vuelve a recorrer todo "categories" y cada elemento de pasa a
+                    GifGrid, este le pone una key y lo agrega al <h3> (Con el nuevo elemento
+                    que fue cargado por useState antes de que haga su renderización)
+            */
+        }           
+        
+    </>
+  )
+}
+
+
+
---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,73 @@
+import { useState } from 'react'
+
+// Como mencioné, este componente unicamente es la barra de escritura
+
+// La persona deberá escribir y una vez toque Enter (onSubmit), deberá devolver el valor cargado
+// a la función OnNewCategory que está dentro del componente principal GifExpertApp
+export const AddCategory = ( { onNewCategory} ) => {
+
+    // 4- Se llama el useState y este le asigna un nuevo valor al inputValue, para
+    // luego re-renderizar el compomente 
+    // (Una vez renderizado inputValue tendrá un nuevo valor, este valor se asigna a "value"
+    // dentro del form)
+    const [ inputValue, setInputValue ] = useState('One punch')
+
+    // 3- Recibimos los cambios del form, desestructuramos el target para conseguir el valor
+    // COMPLETO del formulario (no la letra)
+    const onInputChange = ({ target }) => {
+        setInputValue( target.value )
+        // Al conseguir su valor llamamos el useState
+    }
+
+
+    // 5- Cuando la perosna toca "enter", llama onSubmit
+    // onSubmit devuelve la nueva categoria agregada hacia el componente prinicipal 
+    // (por medio de onNewCategory)
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        // Evita recargar la pagina
+        if( inputValue.trim().length <= 1 ) return;
+        // elimina espacios y verifica si hay caracteres suficientes
+
+        // setCategories( categories => [ ...categories, inputValue ] )
+        onNewCategory( inputValue.trim() )
+        setInputValue('')
+        // Pone el form en blanco una vez enviado
+    }
+
+    // 1- Lo primero es tener un form, (esto es lo unico que se muestra) 
+    // Una vez tenemos el form enfocaremos la logica unicamente en pasar el valor al
+    // tocar "Enter" (onSubmit)
+
+  return (
+
+    <form onSubmit={ onSubmit }>
+        <input
+            type="text"
+            placeholder="Buscar gifs"
+            value={ inputValue } /* El valor que se mostrará en el form es inputValue, este es 
+            enviado mediante el useState */
+
+            //Proceso:
+            /*
+                - Al principio el valor del "form" y "value" es: 'a'
+                - Al tocar una letra, ej 'b', el form se modifica
+                    (onChange) 
+                - Se llama onInputChange
+                - Desestructuramos el target
+                - Extraemos el texto COMPLETO del form
+                - llamamos el useState y cargamos el nuevo inputValue(luego re-renderiza)
+                - en esta nueva "inputValue" otorga un nuevo valor a value. 
+                - Una vez cumplido todo esto podemos modificar el texto del form
+
+            */
+
+            // 2- Se le envia la informacion de los cambios en el form (letra por letra)
+            onChange={ onInputChange } // Cuando hay un cambio en el form se llama onInputChange
+
+    
+        />
+    </form>
+  )
+}
